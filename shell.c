33e613c28030af4ff654f8e6153af06a66f4dae7
jeffdnganga <jeffdaniels950@gmail.com>
2022-05-18 23:38:24 -0700

    Checker fixes

--- shell.c
+++ shell.c
@@ -1,89 +1,220 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * sig_handler - checks if Ctrl C is pressed
- * @sig_num: int
+ * shell - implement shell functionality
+ * @env_list: list of environment variables
+ * @shell_name: name of shell program
+ *
+ * Return: 0 success. Non-zero otherwise
  */
-void sig_handler(int sig_num)
+int shell(list_t *env_list, char *shell_name)
 {
-	if (sig_num == SIGINT)
+	char *input, *full_name;
+	list_t *input_list;
+	char **input_array;
+	int built_ret, exec_ret;
+	char *error_message[4];
+
+	error_message_init(error_message, shell_name, NULL);
+
+	while (1)
 	{
-		_puts("\n#cisfun$ ");
+		input = get_input();
+		if (input == NULL)
+			return (0);
+
+		input_list = split_string(input, " "); /* check input */
+		if (input_list == NULL || input[0] == '\n')
+		{
+			free(input);
+			continue;
+		}
+
+		/* check if input is a built-in command */
+		built_ret = get_built(input_list, shell_name, env_list);
+		if (built_ret != -1)
+		{
+			free_input(input, input_list, NULL);
+			if (built_ret < -1)
+				continue;
+			else
+				return (built_ret);
+		}
+
+		/* check if 1st string is a valid command */
+		error_message[1] = input_list->name;
+		full_name = get_full_name(input_list->name, env_list);
+		if (full_name == NULL)
+		{
+			error_message[2] = "not found";
+			print_error(error_message);
+			free_input(input, input_list, NULL);
+			continue;
+		}
+
+		/* change input_list to input_array */
+		input_array = list_to_array(input_list);
+		if (input_array == NULL)
+		{
+			error_message[2] = "malloc failed";
+			print_error(error_message);
+			free(full_name);
+			free_input(input, input_list, NULL);
+			continue;
+		}
+
+		/* execute command */
+		exec_ret = execute(input_array, full_name, shell_name);
+		if (exec_ret < 0)
+		{
+			free_input(input, input_list, input_array);
+			free(full_name);
+			return (-exec_ret);
+		}
+
+		free(full_name);
+		free_input(input, input_list, input_array);
 	}
+
+	return (0);
 }
 
 /**
-* _EOF - handles the End of File
-* @len: return value of getline function
-* @buff: buffer
+ * get_input - gets input from the terminal
+ *
+ * Return: string input. Otherwise NULL
  */
-void _EOF(int len, char *buff)
+char *get_input(void)
 {
-	(void)buff;
-	if (len == -1)
+	char *buffer = NULL;
+	size_t bufferSize = 0;
+
+	if (isatty(STDIN_FILENO))
+		prompt();
+
+	if (getline(&buffer, &bufferSize, stdin) == -1)
 	{
 		if (isatty(STDIN_FILENO))
-		{
-			_puts("\n");
-			free(buff);
-		}
-		exit(0);
-	}
-}
-/**
-  * _isatty - verif if terminal
-  */
+			write(STDOUT_FILENO, "\n", 2);
 
-void _isatty(void)
-{
-	if (isatty(STDIN_FILENO))
-		_puts("#cisfun$ ");
+		free(buffer);
+		return (NULL);
+	}
+
+	/* replace tabs with spaces */
+	str_rep(buffer, '\t', ' ');
+	
+	return (_strlen(buffer) == 1 ? buffer : _strtok(buffer, "\n"));
 }
+
 /**
- * main - Shell
- * Return: 0 on success
+ * print_error - prints given error message to stderr
+ * @error_message: array of strings composing error message
  */
-
-int main(void)
+void print_error(char **error_message)
 {
-	ssize_t len = 0;
-	char *buff = NULL, *value, *pathname, **arv;
-	size_t size = 0;
-	list_path *head = '\0';
-	void (*f)(char **);
+	int i;
+	char *str;
 
-	signal(SIGINT, sig_handler);
-	while (len != EOF)
+	for (i = 0; i < 4; i++)
 	{
-		_isatty();
-		len = getline(&buff, &size, stdin);
-		_EOF(len, buff);
-		arv = splitstring(buff, " \n");
-		if (!arv || !arv[0])
-			execute(arv);
-		else
+		str = error_message[i];
+
+		if (str)
 		{
-			value = _getenv("PATH");
-			head = linkpath(value);
-			pathname = _which(arv[0], head);
-			f = checkbuild(arv);
-			if (f)
-			{
-				free(buff);
-				f(arv);
-			}
-			else if (!pathname)
-				execute(arv);
-			else if (pathname)
-			{
-				free(arv[0]);
-				arv[0] = pathname;
-				execute(arv);
-			}
+			if (i > 0)
+				write(STDERR_FILENO, ": ", 3);
+
+			write(STDERR_FILENO, str, _strlen(str) + 1);
+
+			if (i == 0)
+				write(STDERR_FILENO, ": 1", 4);
 		}
 	}
-	free_list(head);
-	freearv(arv);
-	free(buff);
+
+	write(STDERR_FILENO, "\n", 2);
+}
+
+/**
+ * prompt - displays shell prompt
+ */
+void prompt(void)
+{
+	char *prompt = "$ ";
+
+	write(STDOUT_FILENO, prompt, _strlen(prompt) + 1);
+}
+
+/**
+ * free_input - frees given memory buffers
+ * @input: string representing input
+ * @input_list: list representing input
+ * @input_array: array of strings representing input
+ */
+void free_input(char *input, list_t *input_list, char **input_array)
+{
+	if (input)
+		free(input);
+
+	if (input_list)
+		free_list(input_list);
+
+	if (input_array)
+		free_array(input_array);
+}
+
+/**
+ * execute - executes a given input command
+ * @input_array: array of strings containing input command
+ * @command: name of command to execute
+ * @shell_name: name of shell program
+ *
+ * Return: 0 success. Otherwise negative integer
+ */
+int execute(char **input_array, char *command, char *shell_name)
+{
+	pid_t child_pid;
+	int status;
+	char *error_message[4];
+
+	error_message_init(error_message, shell_name, command);
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		error_message[2] = "fork error";
+		print_error(error_message);
+		return (-1);
+	}
+	else if (child_pid == 0) /* execute command */
+	{
+		if (execve(command, input_array, NULL) == -1)
+		{
+			error_message[2] = "execve error";
+			print_error(error_message);
+			return (-2);
+		}
+	}
+	else
+	{
+		wait(&status);
+
+		if (WIFEXITED(status))
+			return (WEXITSTATUS(status));
+	}
+
 	return (0);
+}
+
+/** error_message_init - initialize error_message array with given values
+ * @error_message: error message array
+ * @shell_name: 1st value
+ * @command: 2nd value
+ */
+void error_message_init(char **error_message, char *shell_name, char *command)
+{
+	error_message[0] = shell_name;
+	error_message[1] = command;
+	error_message[2] = NULL;
+	error_message[3] = NULL;
 }
